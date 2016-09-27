2891b3bb16865bf8ace828f941c4d330307d22e3
MBODM <webmaster@mbodm.com>
2016-09-27 14:15:33 +0200

    Project temporary deleted

--- CopyPasteOnline/src/WebApp/wwwroot/_references.js
+++
@@ -1,6 +0,0 @@
-﻿/// <autosync enabled="false" />
-/// <reference path="js/home.js" />
-/// <reference path="js/read.js" />
-/// <reference path="js/site.js" />
-/// <reference path="js/write.js" />
-/// <reference path="lib/jquery/dist/jquery.js" />
